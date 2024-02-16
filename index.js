5bb2cfc66ce3c305a6729217e887ddabb39fd60f
ReshadShirzai <302552943@student.rocmondriaan.nl>
2024-02-16 13:48:59 +0100

    express

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
-console.log("Hello!");
+const express = require('express')
+const app = express()
+const port = 3000
 
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-const rl = readline.createInterface({ input, output });
-const passWord = "welkom123";
-
-let answer = await rl.question('What is your name? ');
-
-console.log(`Welcome, ${answer}!`);
-
-answer = await rl.question('Enter your password. ');
-
-if (answer == passWord) {
-    console.log('Password is correct!');
-} else {
-    console.log('Password is incorrect, try again.');
-}
-
-answer = await rl.question('How would you like to utilize NodeJS? Please make sure that your word amount is minimally over 100 words. ');
-
-if (answer == 100) {
-    console.log('Interesting!');
-} else {
-    console.log('Not enough words, please try again.');
-}
-
-rl.close();  
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
