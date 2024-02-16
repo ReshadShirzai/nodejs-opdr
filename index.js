3c7a32a378185afbeb8439c43e7aa8b77be0e688
ReshadShirzai <302552943@student.rocmondriaan.nl>
2024-02-16 14:24:42 +0100

    counter

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const express = require('express')
+import express from "express"
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+let increment = 1;
+let counter = 0;
+
+app.get('/counter', (request, response) => {
+    if (request.query.incrementby) {
+        increment = Number( request.query.incrementby );
+    }
+    counter = counter + increment;
+  response.send('Total: ' + counter)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-});
+})
